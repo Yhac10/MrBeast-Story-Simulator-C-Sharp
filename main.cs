4618579b3a454a75c06f9ec1ef145a09db66eb68
Yhac10 <97116565+Yhac10@users.noreply.github.com>
2023-02-22 13:32:01 +0000

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine("One day, MrBeast gave away...");
+    Console.ReadLine();
+    Console.WriteLine("...to a...");
+    Console.ReadLine();
+    Console.WriteLine("...named...");
+    Console.ReadLine();
+    Console.WriteLine("MrBeast was very...");
+    Console.ReadLine();
+    Console.WriteLine("...when he gave away the...");
+    Console.ReadLine();
+    Console.WriteLine("The end.");
+    
+    
+    
+    
+  }
+}
